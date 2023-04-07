f8fce1e4c9bc6570d8a73b2eea42177f07609357
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-04-07 00:03:10 -0300

    feat: add new chapter

--- Chapter 5/http/main.go
+++ Chapter 5/http/main.go
@@ -3,13 +3,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"io"
+	"time"
+	"encoding/json"
 )
+
+type Family []struct {
+	CreatedAt  time.Time `json:"createdAt"`
+	Name       string    `json:"name"`
+	Job        string    `json:"job"`
+	FamilyName string    `json:"familyName"`
+	ID         string    `json:"id"`
+}
 
 func main() {
 
-	url := "https://google.com.br"
+	url := "https://610aa52552d56400176afebe.mockapi.io/api/v1/friendlist"
 
-	client := &http.client{}
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
 
-	fmt.Println("start")
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	f := Family{}
+	err = json.Unmarshal(body, &f);
+
+	for i, v := range f {
+		fmt.Println("Elemento:", i)
+		fmt.Println(v.Job)
+		fmt.Println(v.Name)
+	}
 }
